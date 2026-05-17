d018464b2af690b9d9ed69dce8f37922219761ef
TherLaf <therlaf0409@gmail.com>
2026-05-17 20:23:01 +0800

    Implemented proxy dhcp with netboot's modified conf.

--- dnsmasq/entrypoint.sh
+++ dnsmasq/entrypoint.sh
@@ -1,25 +1,33 @@
+#!/bin/sh
+
+# Write dnsmasq's configuration file.
+cat <<EOF > /etc/dnsmasq.conf
 # Disable DNS service.
 port=0
 
 # Set the subnet the DHCP listens to.
-dhcp-range=192.168.1.0,proxy
+dhcp-range=${subnet},proxy,255.255.255.0
 log-dhcp
 
 # Configuration from https://netboot.xyz/docs/docker/dhcp
 
 # Standard PC BIOS
 dhcp-match=set:bios,60,PXEClient:Arch:00000
-dhcp-boot=tag:bios,netboot.xyz.kpxe,,SERVER_IP_ADDRESS
+dhcp-boot=tag:bios,netboot.xyz.kpxe,,${pxe_server_ip}
 
 # 64-bit x86 EFI
 dhcp-match=set:efi64,60,PXEClient:Arch:00007
-dhcp-boot=tag:efi64,netboot.xyz.efi,,SERVER_IP_ADDRESS
+dhcp-boot=tag:efi64,netboot.xyz.efi,,${pxe_server_ip}
 
 # 64-bit x86 EFI (obsolete)
 dhcp-match=set:efi64-2,60,PXEClient:Arch:00009
-dhcp-boot=tag:efi64-2,netboot.xyz.efi,,SERVER_IP_ADDRESS
+dhcp-boot=tag:efi64-2,netboot.xyz.efi,,${pxe_server_ip}
 
 # 64-bit UEFI for arm64
 dhcp-match=set:efi64-3,60,PXEClient:Arch:0000B
 dhcp-match=set:efi64-3,60,PXEClient:Arch:00011
-dhcp-boot=tag:efi64-3,netboot.xyz-arm64.efi,,SERVER_IP_ADDRESS
+dhcp-boot=tag:efi64-3,netboot.xyz-arm64.efi,,${pxe_server_ip}
+EOF
+
+# Start dnsmasq.
+dnsmasq --no-daemon
